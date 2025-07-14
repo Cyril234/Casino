7007c4fe5f640eb9e079ec9d4bbbff867eab70ca
King-Kong18 <loris.sigrist@lernende.bbw.ch>
2025-07-14 16:11:00 +0200

    Login with username and password.

--- Documents/Module/Casino/casino-frontend/src/components/login/LoginWithBadge.tsx
+++ Documents/Module/Casino/casino-frontend/src/components/login/LoginWithBadge.tsx
@@ -1,7 +1,8 @@
 export default function LoginWithBadge() {
     return (
         <>
-           
+            <h1>Melde dich mit deinem Badge an!</h1>
+            <p>Halte dafür einfach deinen Badge an den Leser am Automaten!</p>
         </>
     )
 }
