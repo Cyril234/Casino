763e46d399eaf4fb8d1cab9f08a2e0f5b9bafad8
King-Kong18 <loris.sigrist@lernende.bbw.ch>
2025-07-14 15:22:51 +0200

    Skeletton for the tsx-files.

--- Documents/Module/Casino/casino-frontend/src/components/login/LoginAsGuest.tsx
+++ Documents/Module/Casino/casino-frontend/src/components/login/LoginAsGuest.tsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router";
+
 export default function LoginAsGuest() {
+    const navigate = useNavigate();
+
+    const goToGameoverview = () => navigate('/gameoverview');
+
     return (
         <>
             <div>
                 <h1>Login als Gast</h1>
+                <p>Verwende fast alle Funktionen, ohne ein Konto zu erstellen! Du erhälst einen Startbetrag, welchen du verspielen kannst!</p>
+                <button onClick={goToGameoverview}>Als Gast fortfahren</button>
             </div>
         </>
     )
