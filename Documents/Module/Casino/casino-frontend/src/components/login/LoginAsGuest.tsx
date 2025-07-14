50aaad32ef7aeb7faaf3689cd677a4598e323e4f
King-Kong18 <loris.sigrist@lernende.bbw.ch>
2025-07-14 14:15:53 +0200

    Groundbuilding from the Loginpages.

---
+++ Documents/Module/Casino/casino-frontend/src/components/login/LoginAsGuest.tsx
@@ -0,0 +1,10 @@
+export default function LoginAsGuest() {
+    return (
+        <>
+            <div>
+                <h1>Login als Gast</h1>
+            </div>
+        </>
+    )
+
+}
---
+++ Documents/Module/Casino/casino-frontend/src/components/login/Loginoverview.tsx
@@ -0,0 +1,18 @@
+export default function Loginoverview() {
+    return (
+        <>
+            <h1>Anmeldevariante wählen...</h1>
+            <div>
+                <button>
+                    Anmeldung mit Email und Passwort
+                </button>
+                <button>
+                    Anmeldung mit Badge
+                </button>
+                <button>
+                    Anmeldung als Gast
+                </button>
+            </div>
+        </>
+    )
+}
